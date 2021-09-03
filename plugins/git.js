ea40f94e548ee77c326f9e860b5d5d12aeb17fa2
Chinju-Mwol <89919773+ajuajnas@users.noreply.github.com>
2021-09-03 10:22:19 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -19,7 +19,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater salman dx*
-*owner number wa.me/972 52-545-8878*
+*owner number wa.me/917736936605*
 *bot setting video : https://youtu.be/_D4ZYuUSXjs
 *githublink       https://github.com/ajuajnas/lisa*
 *audio commads    https://github.com/ajuajnas/lisa/tree/master/uploads*
